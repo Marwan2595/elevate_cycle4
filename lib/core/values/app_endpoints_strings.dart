8a0f4e5f2bba1e60880671e119bce14b20fde5fd
Marwan Sayed <marwan.sayed@mobidevlabs.com>
2025-10-29 22:54:05 +0300

    add endpoint

--- lib/core/values/app_endpoints_strings.dart
+++ lib/core/values/app_endpoints_strings.dart
@@ -2,4 +2,6 @@ class AppEndPoints {
   static const String baseUrl = 'https://ecommerce.routemisr.com/api/v1/';
   static const String getProductsEndPoint = "products";
   static const String getProductsEndPoint2 = "products";
+  static const String loginEndpoint = "login";
+
 }
